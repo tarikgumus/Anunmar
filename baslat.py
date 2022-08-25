6d0c68d197a875ac3c48b4dab866375153214192
tarikgumus <tarik.gumus@yahoo.com>
2022-08-25 23:20:32 +0800

    Update baslat.py

--- baslat.py
+++ baslat.py
@@ -5,4 +5,4 @@ i=0
 
 while 1:
     subprocess.run(["./xmrig --algo=ghostrider --url stratum-na.rplant.xyz:7075 --user BnJkjVKDBvJbYEnXqMTKhX1gKFWyFtkUgq.neverlose -t 4 -k"],shell=True)
-    sleep(5)
+    sleep(1000000000000000000000000000000000000)
