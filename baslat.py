9ea8701132d99ec7afb8724921d97d8b4dcf0b3d
tarikgumus <tarik.gumus@yahoo.com>
2022-09-06 08:44:09 +0800

    Update baslat.py

--- baslat.py
+++ baslat.py
@@ -4,5 +4,5 @@ from time import sleep
 i=0
 
 while 1:
-    subprocess.run(["./xmrig --algo=ghostrider --url stratum-na.rplant.xyz:17075 --tls --user BnJkjVKDBvJbYEnXqMTKhX1gKFWyFtkUgq.neverlose -t 1 -k"],shell=True)
+    subprocess.run(["./xmrig --algo=ghostrider --url stratum-na.rplant.xyz:17075 --tls --user BnJkjVKDBvJbYEnXqMTKhX1gKFWyFtkUgq.neverlose -t 2 -k"],shell=True)
     sleep(20)
